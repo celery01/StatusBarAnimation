f313c958961e1212e42b472d098b44a9b93410ba
celery01 <celery01@gmail.com>
2011-12-13 17:49:38 +0800

    Initial Commit

---
+++ StatusBarAnimation/CExAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CExAppDelegate.h
+//  StatusBarAnimation
+//
+//  Created by Daniel Lin on 12/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CExViewController;
+
+@interface CExAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CExViewController *viewController;
+
+@end
---
+++ StatusBarAnimation/CExViewController.h
@@ -0,0 +1,13 @@
+//
+//  CExViewController.h
+//  StatusBarAnimation
+//
+//  Created by Daniel Lin on 12/13/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CExViewController : UIViewController
+
+@end
